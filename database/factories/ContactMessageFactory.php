008edafa56004115e97c3e4b5e49921a149038bc
Thinh Le <lethinh73@hotmail.com>
2025-05-31 00:20:11 -0500

    Working on the model for contact message

---
+++ database/factories/ContactMessageFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ContactMessage>
+ */
+class ContactMessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->safeEmail(),
+            'message' => fake()->realText()
+        ];
+    }
+}
