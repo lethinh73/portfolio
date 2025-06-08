59126340f1baa218a712d6ff4869ceac6f26c587
Thinh Le <lethinh73@hotmail.com>
2025-06-07 23:17:01 -0500

    Remove debug

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,6 @@ use App\Models\ContactMessage;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schedule;
 
@@ -15,8 +14,6 @@ Artisan::command('inspire', function () {
 
 Schedule::call(function () {
     $newMessages = ContactMessage::whereDate('created_at', Carbon::yesterday())->get();
-
-    Log::debug($newMessages);
 
     if ($newMessages->isNotEmpty()) {
         Mail::to('lethinh73@gmail.com')->send(new MessagesDailyReport($newMessages));
