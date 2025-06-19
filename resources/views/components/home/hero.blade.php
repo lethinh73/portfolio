9370679738f27b6e2fd0e2fb5faf38fa16906b3d
Thinh Le <lethinh73@hotmail.com>
2025-06-19 15:43:45 -0500

    Seperating codes into components + creating guest layout

---
+++ resources/views/components/home/hero.blade.php
@@ -0,0 +1,115 @@
+<div class="bg-gray-900 relative isolate pt-14">
+    <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+        <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-288.75"
+            style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+        </div>
+    </div>
+
+    <div class="py-10 sm:py-15 lg:pb-20">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl text-center">
+                <h1 class="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">Thinh Le
+                </h1>
+                <h3 class="text-2xl font-semibold tracking-tight text-balance text-white sm:text-5xl">Software
+                    Developer</h3>
+
+                <div class="w-full flex justify-center mt-5">
+                    <img class="transform-3d size-1/4 rounded-full shadow-xl border-2 border-amber-200 animate-wiggle"
+                        src="/images/avatar.jpg" />
+                </div>
+
+                <p class="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+                    Accomplished and detail-oriented Software Development
+                    Professional with 5+ years of experience in building scalable
+                    applications for real estate, education, finance, and logistics
+                    industries. Passionate about creating innovative, efficient
+                    digital solutions.
+                </p>
+                <div class="mt-10 flex items-center justify-center gap-x-6">
+                    <a href="#"
+                        class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">See
+                        Resume</a>
+                    <a href="#" class="text-sm/6 font-semibold text-white">Learn more <span
+                            aria-hidden="true">→</span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex justify-center" id="experience">
+        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+            <div
+                class="relative overflow-hidden rounded-lg bg-gray-700 px-4 pt-5 pb-12 shadow-sm sm:px-6 sm:pt-6 border-2 border-gray-200">
+                <dt>
+                    <div class="absolute rounded-md bg-indigo-500 p-3">
+                        <svg class="size-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" aria-hidden="true" data-slot="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                        </svg>
+                    </div>
+                    <p class="ml-16 truncate text-sm font-medium text-gray-200">Total Subscribers</p>
+                </dt>
+                <dd class="ml-16 flex items-baseline pb-6 sm:pb-7">
+                    <p class="text-2xl font-semibold text-gray-400">71,897 %</p>
+                    <p class="ml-2 flex items-baseline text-sm font-semibold text-green-600">
+                        <svg class="size-5 shrink-0 self-center text-green-500" viewBox="0 0 20 20" fill="currentColor"
+                            aria-hidden="true" data-slot="icon">
+                            <path fill-rule="evenodd"
+                                d="M10 17a.75.75 0 0 1-.75-.75V5.612L5.29 9.77a.75.75 0 0 1-1.08-1.04l5.25-5.5a.75.75 0 0 1 1.08 0l5.25 5.5a.75.75 0 1 1-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0 1 10 17Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="sr-only"> Increased by </span>
+                        122%
+                    </p>
+                    <div class="absolute inset-x-0 bottom-0 bg-gray-800 px-4 py-4 sm:px-6">
+                        <div class="text-sm">
+                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">View all<span
+                                    class="sr-only"> Total Subscribers stats</span></a>
+                        </div>
+                    </div>
+                </dd>
+            </div>
+
+            <div
+                class="relative overflow-hidden rounded-lg bg-gray-700 px-4 pt-5 pb-12 shadow-sm sm:px-6 sm:pt-6 border-2 border-gray-200">
+                <dt>
+                    <div class="absolute rounded-md bg-indigo-500 p-3">
+                        <svg class="size-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" aria-hidden="true" data-slot="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                        </svg>
+                    </div>
+                    <p class="ml-16 truncate text-sm font-medium text-gray-200">Total Subscribers</p>
+                </dt>
+                <dd class="ml-16 flex items-baseline pb-6 sm:pb-7">
+                    <p class="text-2xl font-semibold text-gray-400">71,897 %</p>
+                    <p class="ml-2 flex items-baseline text-sm font-semibold text-green-600">
+                        <svg class="size-5 shrink-0 self-center text-green-500" viewBox="0 0 20 20" fill="currentColor"
+                            aria-hidden="true" data-slot="icon">
+                            <path fill-rule="evenodd"
+                                d="M10 17a.75.75 0 0 1-.75-.75V5.612L5.29 9.77a.75.75 0 0 1-1.08-1.04l5.25-5.5a.75.75 0 0 1 1.08 0l5.25 5.5a.75.75 0 1 1-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0 1 10 17Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="sr-only"> Increased by </span>
+                        122%
+                    </p>
+                    <div class="absolute inset-x-0 bottom-0 bg-gray-800 px-4 py-4 sm:px-6">
+                        <div class="text-sm">
+                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">View all<span
+                                    class="sr-only"> Total Subscribers stats</span></a>
+                        </div>
+                    </div>
+                </dd>
+            </div>
+        </dl>
+    </div>
+
+    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true">
+        <div class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-288.75"
+            style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+        </div>
+    </div>
+</div>
