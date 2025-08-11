9f7aa1ad8338f83aa3ece51359e4eb86304af2ce
Thinh Le <lethinh73@hotmail.com>
2025-08-11 00:03:12 -0500

    Adding backend slim 4

---
+++ backend/app/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->get('/', function (Request $request, Response $response) {
+        // Return a simple json
+        $response->getBody()->write(json_encode(['message' => 'Hello world!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+};
