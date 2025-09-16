e056d69377237fb52ed39e73f281219e6e318c10
Thinh Le <lethinh73@hotmail.com>
2025-09-15 20:47:25 -0500

    Adding styling

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,12 +1,10 @@
 import { Head } from '@inertiajs/react';
 
 export default function Home() {
-    return (
-        <>
-            <Head title="Home" />
-            <div>
-                Home
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Head title="Home" />
+      
+    </>
+  );
 }
