a8b66d88c75bfef9a87bb1274f31ed4f6be39260
Thinh Le <lethinh73@hotmail.com>
2025-10-22 22:57:57 -0500

    WIP Home page

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -8,17 +8,20 @@ export default function Home() {
             <div className="flex flex-col items-center justify-center">
                 <div className="mt-4">
                     <h1 className="text-4xl font-bold">
-                        Welcome to the Home Page
+                        Hello, I'm Thinh Le
                     </h1>
+                    <h2 className="text-2xl mt-2">
+                        A Full-Stack Developer
+                    </h2>
                 </div>
                 <div className="mb-4">
-                    <p className="text-lg">This is the home page content.</p>
+                    <img src="/images/profile.jpg" alt="Profile Picture" className="w-48 h-48 rounded-full mt-4" />
                 </div>
-                <div className="grid w-full max-w-4xl grid-cols-3 gap-4">
-                    <div className="col-span-1 flex border-2 border-white">
-                        Yo1
+                <div className="grid w-[80%] grid-cols-1 sm:grid-cols-3 gap-4">
+                    <div className="col-span-1 flex border-2 border-white w-full">
+                        
                     </div>
-                    <div className="col-span-2 flex border-2 border-white">
+                    <div className="col-span-1 sm:col-span-2 flex border-2 border-white w-full">
                         Yo2
                     </div>
                 </div>
